8bf5bacb43804f2149631658a746463738d553af
gityongli <2508112607@qq.com>
2017-12-27 17:51:35 +0800

    66666

--- js/taobao.js
+++ js/taobao.js
@@ -1,3 +1,15 @@
-function run(){
-    document.getElementById()
+function li1() {
+    document.getElementById("hua").style.transform = "translateX(0rem)";
+}
+function li2() {
+    document.getElementById("hua").style.transform = "translateX(155px)";
+}
+function li3() {
+    document.getElementById("hua").style.transform = "translateX(4.133rem)";
+}
+function li4() {
+    document.getElementById("hua").style.transform = "translateX(6.198rem)";
+}
+function li5() {
+    document.getElementById("hua").style.transform = "translateX(8.264rem)";
 }
