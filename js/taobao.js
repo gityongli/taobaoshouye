09a0826bf52bd37a02b378c3104b2ff3c86644fa
gityongli <2508112607@qq.com>
2017-12-27 18:24:16 +0800

    111000

--- js/taobao.js
+++ js/taobao.js
@@ -1,15 +1,17 @@
 function li1() {
-    document.getElementById("hua").style.transform = "translateX(0rem)";
+    document.getElementById("hover").style.transform = "translateX(0px)";
 }
 function li2() {
-    document.getElementById("hua").style.transform = "translateX(155px)";
+    document.getElementById("hover").style.transform = "translateX(1.7rem)";
 }
 function li3() {
-    document.getElementById("hua").style.transform = "translateX(4.133rem)";
+    document.getElementById("hover").style.transform = "translateX(3.4rem)";
 }
 function li4() {
-    document.getElementById("hua").style.transform = "translateX(6.198rem)";
+    document.getElementById("hover").style.transform = "translateX(5.1rem)";
 }
 function li5() {
-    document.getElementById("hua").style.transform = "translateX(8.264rem)";
+    document.getElementById("hover").style.transform = "translateX(6.8rem)";
 }
+
+
